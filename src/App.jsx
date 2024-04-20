abcab499d90a7cc8aad9f8e556d03824742ca1e5
Ammari045 <Ammari045@users.noreply.github.com>
2024-04-20 18:28:35 +0500

    environment set for devloping app

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
-import ShoppingCart from "./components/ShoppingCart";
-import { useEffect } from "react";
-import { fetchUser } from "./features/thunk/userThunk";
+import GithubApiComp from "./components/githubApi/GithubApiComp";
 
 export default function App() {
-  // const { isLoading } = useSelector((state) => state.cart);
-  const { isLoading, user } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  // USING THUNK
-  useEffect(() => {
-    dispatch(fetchUser());
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="loading">
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-
   return (
     <>
-      {/* <ShoppingCart /> */}
-      <div className="flex min-w-60 flex-wrap justify-center items-center">
-        {user.map((u) => {
-          return (
-            <div
-              key={u.id}
-              className="bg-white shadow-[0_8px_12px_-6px_rgba(0,0,0,0.2)] border p-2 w-full max-w-sm rounded-lg font-[sans-serif] overflow-hidden mx-auto mt-4"
-            >
-              <img
-                src="https://readymadeui.com/cardImg.webp"
-                className="w-full rounded-lg"
-              />
-              <div className="px-4 my-6 text-center">
-                <h3 className="text-lg font-semibold">{u.name}</h3>
-                <p className="mt-2 text-sm text-gray-400">{u.username}</p>
-                <button
-                  type="button"
-                  className="px-6 py-2 w-full mt-4 rounded-lg text-white text-sm tracking-wider font-semibold border-none outline-none bg-blue-600 hover:bg-blue-700 active:bg-blue-600"
-                >
-                  {u.phone}
-                </button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <header className="bg-blue-600 text-light py-3 text-center">
+        <h1 className="text-5xl italic ">Github Api With Redux Toolkit</h1>
+      </header>
+      <main className="max-w-[1000px] min-h-[500px] mx-auto">
+        <GithubApiComp />
+      </main>
+      <footer className="bg-red-700 text-center py-5">
+        <h1 className="text-2xl italic">Copy Rights 2024 </h1>
+      </footer>
     </>
   );
 }
---
+++ src/components/githubApi/GithubApiComp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GithubApiComp() {
+  return (
+    <>
+      <h1 className="text-xl">HELLO WORLD</h1>
+    </>
+  );
+}
